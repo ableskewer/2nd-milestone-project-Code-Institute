ab3f86af5f336a09fac2ac012b8f9d5c41d1b9b7
ableskewer <danse4@msn.com>
2020-05-22 10:07:09 +0000

    just a regular save, not much progress

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Helps:
 https://stackoverflow.com/questions/1179928/how-can-i-put-a-vertical-line-down-the-center-of-a-div
 www.getbootstrap.com
 www.w3schools.com
+https://www.tablesgenerator.com/html_tables#
+https://support.imcreator.com/hc/en-us/articles/232392888-Creating-a-Facebook-share-link-on-your-page
+https://stackoverflow.com/questions/13437446/how-to-display-selected-item-in-bootstrap-button-dropdown-title
+https://stackoverflow.com/questions/2906582/how-to-create-an-html-button-that-acts-like-a-link
+https://til.hashrocket.com/posts/v2s2gxgifj-submit-a-form-with-a-button-outside-the-form
+https://stackoverflow.com/questions/37736056/button-type-button-vs-submit/37736165
+
+to be used
+https://myclabs.github.io/jquery.confirm/
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,57 @@
-// Navbar
-
+// Navbar courtesy of Bootstrap4
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
     document.getElementById("navbar").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
+};
+
+// THE MODAAAAAL
+
+// Get the modal and the open-modal-button and the day you wish to mark
+var modal = document.getElementById("modal");
+var btns = document.getElementsByClassName("today");
+var btnsLength = btns.length;
+var day = document.getElementsByClassName("today");
+
+// Get the appropriate color butten
+var green = document.getElementById("greenOK");
+var yellow = document.getElementById("yellowOK");
+var red = document.getElementById("redOK");
+var clear = document.getElementById("clear-color");
+
+// When any day is clicked the modal appears
+for (var i = 0; i < btnsLength; i++) {
+  btns[i].onclick = function () {
+    modal.style.display = "block";
+  };
+  // Marking the color on the day with one of the buttons
+  green.onclick = function () {
+    day[0].style.backgroundColor = "#90EE90";
+    modal.style.display = "none";
+  };
+  yellow.onclick = function () {
+    day[0].style.backgroundColor = "#FFFF33";
+    modal.style.display = "none";
+  };
+  red.onclick = function () {
+    day.style.backgroundColor = "#FF2B00";
+    modal.style.display = "none";
+  };
+
+  clear.onclick = function () {
+    day.style.backgroundColor = "";
+    modal.style.display = "none";
+  };
 }
 
-// Dropdown selectors
-
-
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
